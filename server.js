4911b1ec85466c3659f72a6cfa27a55d37b85e2f
mykas <mykolass@wix.com>
2019-10-14 16:43:40 +0300

    Remove

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const tls = require('tls');
 
 var send = require('gmail-send')({
   //var send = require('../index.js')({
-  user: 'mykolas.sindeikis@gmail.com',
+  user: 'target email',
   // user: credentials.user,                  // Your GMail account used to send emails
   pass: 'special-password',
   // pass: credentials.pass,                  // Application-specific password
-  to: 'mykolas.sindeikis@gmail.com',
+  to: 'source email',
   // to:   credentials.user,                  // Send to yourself
   // you also may set array of recipients:
   // [ 'user1@gmail.com', 'user2@gmail.com' ]
   // from:    credentials.user,            // from: by default equals to user
   // replyTo: credentials.user,            // replyTo: by default undefined
   // bcc: 'some-user@mail.com',            // almost any option of `nodemailer` will be passed to it
-  subject: 'Pilaites Butu pokytis',
+  subject: 'subject',
   text: 'gmail-send example 1', // Plain text
   //html:    '<b>html text</b>'            // HTML
 });
